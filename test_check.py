6fe199094e379d8fc1730ad97231cceb0e03dfde
Obrepal <48407260+Obrepal@users.noreply.github.com>
2022-01-08 21:14:49 +0100

    Update test_check.py

--- test_check.py
+++ test_check.py
@@ -8,6 +8,6 @@ def test_repos():
     response_repos = client.get("/user/Obrepal/repos")
     assert response_repos.status_code == 200
 
-#def test_repos():
-   # response_repos = client.get("/user/Obrepal/bytes")
-    #assert response_repos.status_code == 200
+def test_bytes():
+   response_bytes = client.get("/user/Obrepal/bytes")
+   assert response_bytes.status_code == 200
