1f9bbd713d6422c6cdb15ccef5770d766f3242cb
Obrepal <48407260+Obrepal@users.noreply.github.com>
2022-01-09 20:18:26 +0100

    better tests

--- test_check.py
+++ test_check.py
@@ -4,10 +4,26 @@ from working import app
 
 client = TestClient(app)
 
+
 def test_repos():
     response_repos = client.get("/user/Obrepal/repos")
-    assert response_repos.status_code == 200
+    if response_repos.status_code == 500:
+        assert response_repos.json() == {'detail': 'Item not found'}
+    else:
+        assert response_repos.status_code == 200
 
 def test_stars():
    response_stars = client.get("/user/Obrepal/stars")
-   assert response_stars.status_code == 200
+   if response_stars.status_code == 500:
+    assert response_stars.json() == {'detail': 'Item not found'}
+   else: 
+    assert response_stars.status_code == 200
+
+def test_stars_quantity():
+   response_stars = client.get("/user/Obrepal/stars")
+   if response_stars.status_code == 500:
+    assert response_stars.json() == {'detail': 'Item not found'}
+   else: 
+    assert   response_stars.json() == {"Total_stars": 0}
+
+    #{"Bytes in given language":{"Python":[2,35184],"C":[1,17047],"Makefile":[1,192],"Shell":[1,102],"MATLAB":[1,22632],"Java":[1,34921]}}
